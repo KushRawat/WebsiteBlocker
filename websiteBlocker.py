63fda23a78ec60f358f8156f992973e9ca3f5109
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-07-18 07:08:55 +0530

    adding comments

--- websiteBlocker.py
+++ websiteBlocker.py
@@ -2,14 +2,15 @@ import time
 from datetime import datetime as dt
 
 hostsTemp = "hosts"
-hostsPath = "C:\Windows\System32\drivers\etc\hosts"
+hostsPath = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 websiteList = ["www.netflix.com", "netflix.com"]
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 21):
         print("Working hours...")
-        with open(hostsTemp, 'r+') as file:
+        # Using reading and appending file method
+        with open(hostsTemp, 'r+') as file: 
             content = file.read()
             for website in websiteList:
                 if website in content:
@@ -17,9 +18,10 @@ while True:
                 else:
                     file.write(redirect+" "+website+"\n")
     else:
+        # Using reading and appending file method
         with open(hostsTemp, 'r+') as file:
             content = file.readlines()
-            file.seek(0)
+            file.seek(0) # Placing pointer to the top of the file
             for line in content:
                 if not any(website in line for website in websiteList):
                     file.write(line)
